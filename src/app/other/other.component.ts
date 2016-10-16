784c5c6d55fbbac84c2f119015e4559ea64727a5
shadab ahmed <shadab33@hotmail.com>
2016-10-16 23:51:00 +0500

    first

--- src/app/other/other.component.ts
+++ src/app/other/other.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-other',
+  selector: 'my-puzzle',
   templateUrl: './other.component.html',
   styleUrls: ['./other.component.css']
 })
 export class OtherComponent implements OnInit {
 
   constructor() { }
+switch1Number:number;
+switch2Number:number;
+switch3Number:number;
+switch4Number:number;
 
-  ngOnInit() {
-  }
+ngOnInit(){
+this.switch1Number =Math.round(Math.random());
+this.switch2Number =Math.round(Math.random());
+this.switch3Number =Math.round(Math.random());
+this.switch4Number =Math.round(Math.random());
+console.log(this.switch1Number,this.switch2Number,this.switch3Number,this.switch4Number);
+}
 
 }
